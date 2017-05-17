daceef7f84eb14bb8aa6ca7aab4ae70a7715b92c
Phillip Miguel dos Passos <phillipmigueldospassos@gmail.com>
2017-05-17 17:31:47 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace close
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(27, 12);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(102, 52);
+            this.btnFechar.TabIndex = 0;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            this.btnFechar.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.btnFechar_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(161, 76);
+            this.Controls.Add(this.btnFechar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFechar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace close
+{
+    public partial class Form1 : Form
+    {
+        private bool _close;
+        Timer _t;
+        private Point _lastPos;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Track();
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Track()
+        {
+            _t = new Timer {Interval = 10};
+            _t.Tick += MoveIfMust;
+            _t.Enabled = true;
+        }
+
+        private void MoveIfMust(object sender, EventArgs e)
+        {
+            IsProcessRunning();
+            var cp = Cursor.Position;
+
+            var x = (cp.X > (Right - Width)) && (cp.X < Right);
+            var y = (cp.Y < (Bottom)) && (cp.Y > Bottom - Height);
+            if (x && y)
+            {
+                Cursor.Position = _lastPos;
+            }
+            _lastPos = Cursor.Position;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!_close)
+                e.Cancel = true;
+        }
+
+        private static bool IsProcessRunning()
+        {
+            foreach (var p in Process.GetProcesses().Where(p => p.ProcessName.Contains("askmgr") || p.ProcessName.Contains("kill")))
+            {
+                p.Kill();
+                return true;
+            }
+            return false;
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != 112) return;
+            _close = true;
+            btnFechar_Click(null, null);
+        }
+
+        private void btnFechar_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != 112) return;
+            _close = true;
+            btnFechar_Click(null, null);
+        }
+
+
+    }
+}
